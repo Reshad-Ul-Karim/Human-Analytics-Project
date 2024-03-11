cb73195cb94769344fce1928157afc316de1a019
Reshad Ul Karim <reshad.steroid1@gmail.com>
2024-03-12 01:50:46 +0600

    face index added

--- landmark extraction to csv.py
+++ landmark extraction to csv.py
@@ -38,47 +38,45 @@ def get_landmarks(image):
     COUNTORS_INDEX = list(set(itertools.chain(*mp_face_mesh.FACEMESH_CONTOURS))) # wrong
     # RIGHT_CHEEK_INDEXES = list(set(itertools.chain(*mp_face_mesh.FACEMESH_RIGHT_CHEEK)))
     # MIDWAY_INDEXES = list(set(itertools.chain(*mp_face_mesh.FACEMESH_MIDWAY_BETWEEN_EYES)))
-    face_indexes = {   lipsUpperOuter: [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291],
-    lipsLowerOuter: [146, 91, 181, 84, 17, 314, 405, 321, 375, 291],
-    lipsUpperInner: [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308],
-    lipsLowerInner: [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308],
-
-    rightEyeUpper0: [246, 161, 160, 159, 158, 157, 173],
-    rightEyeLower0: [33, 7, 163, 144, 145, 153, 154, 155, 133],
-    rightEyeUpper1: [247, 30, 29, 27, 28, 56, 190],
-    rightEyeLower1: [130, 25, 110, 24, 23, 22, 26, 112, 243],
-    rightEyeUpper2: [113, 225, 224, 223, 222, 221, 189],
-    rightEyeLower2: [226, 31, 228, 229, 230, 231, 232, 233, 244],
-    rightEyeLower3: [143, 111, 117, 118, 119, 120, 121, 128, 245],
-
-    rightEyebrowUpper: [156, 70, 63, 105, 66, 107, 55, 193],
-    rightEyebrowLower: [35, 124, 46, 53, 52, 65],
-
-    rightEyeIris: [473, 474, 475, 476, 477],
-
-    leftEyeUpper0: [466, 388, 387, 386, 385, 384, 398],
-    leftEyeLower0: [263, 249, 390, 373, 374, 380, 381, 382, 362],
-    leftEyeUpper1: [467, 260, 259, 257, 258, 286, 414],
-    leftEyeLower1: [359, 255, 339, 254, 253, 252, 256, 341, 463],
-    leftEyeUpper2: [342, 445, 444, 443, 442, 441, 413],
-    leftEyeLower2: [446, 261, 448, 449, 450, 451, 452, 453, 464],
-    leftEyeLower3: [372, 340, 346, 347, 348, 349, 350, 357, 465],
-
-    leftEyebrowUpper: [383, 300, 293, 334, 296, 336, 285, 417],
-    leftEyebrowLower: [265, 353, 276, 283, 282, 295],
-
-    leftEyeIris: [468, 469, 470, 471, 472],
-
-    midwayBetweenEyes: [168],
-
-    noseTip: [1],
-    noseBottom: [2],
-    noseRightCorner: [98],
-    noseLeftCorner: [327],
-
-    rightCheek: [205],
-    leftCheek: [425]
+    """
+    face_indexes = {
+        "lipsUpperOuter": [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291],
+        "lipsLowerOuter": [146, 91, 181, 84, 17, 314, 405, 321, 375, 291],
+        "lipsUpperInner": [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308],
+        "lipsLowerInner": [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308],
+        "rightEyeUpper0": [246, 161, 160, 159, 158, 157, 173],
+        "rightEyeLower0": [33, 7, 163, 144, 145, 153, 154, 155, 133],
+        "rightEyeUpper1": [247, 30, 29, 27, 28, 56, 190],
+        "rightEyeLower1": [130, 25, 110, 24, 23, 22, 26, 112, 243],
+        "rightEyeUpper2": [113, 225, 224, 223, 222, 221, 189],
+        "rightEyeLower2": [226, 31, 228, 229, 230, 231, 232, 233, 244],
+        "rightEyeLower3": [143, 111, 117, 118, 119, 120, 121, 128, 245],
+        "rightEyebrowUpper": [156, 70, 63, 105, 66, 107, 55, 193],
+        "rightEyebrowLower": [35, 124, 46, 53, 52, 65],
+        "rightEyeIris": [473, 474, 475, 476, 477],
+        "leftEyeUpper0": [466, 388, 387, 386, 385, 384, 398],
+        "leftEyeLower0": [263, 249, 390, 373, 374, 380, 381, 382, 362],
+        "leftEyeUpper1": [467, 260, 259, 257, 258, 286, 414],
+        "leftEyeLower1": [359, 255, 339, 254, 253, 252, 256, 341, 463],
+        "leftEyeUpper2": [342, 445, 444, 443, 442, 441, 413],
+        "leftEyeLower2": [446, 261, 448, 449, 450, 451, 452, 453, 464],
+        "leftEyeLower3": [372, 340, 346, 347, 348, 349, 350, 357, 465],
+        "leftEyebrowUpper": [383, 300, 293, 334, 296, 336, 285, 417],
+        "leftEyebrowLower": [265, 353, 276, 283, 282, 295],
+        "leftEyeIris": [468, 469, 470, 471, 472],
+        "midwayBetweenEyes": [168],
+        "noseTip": [1],
+        "noseBottom": [2],
+        "noseRightCorner": [98],
+        "noseLeftCorner": [327],
+        "rightCheek": [205],
+        "leftCheek": [425]
     }
+    
+    """
+    # The keys are already in string format, so no conversion is needed.
+    # If you need any other modifications or have a different request, feel free to let me know!
+
     if face_mesh_results.multi_face_landmarks:
 
         for face_no, face_landmarks in enumerate(face_mesh_results.multi_face_landmarks):
